f7280b2e3a5b34700d11f92ff76975c8bb0d50a7
Angel Yankov <yankov-hw@protonmail.com>
2021-09-04 01:48:23 +0300

    Disable DB connection on master temporarily

--- webapp/app_base.py
+++ webapp/app_base.py
@@ -1,66 +1,59 @@
 from flask.json import jsonify
 from flask import Flask,render_template, request
-from our_utils import *
+#from our_utils import *
 
 # This is basically a routing table for all the URIs
-# everything resides either in communism_utils or separate modules
+# everything resides either in utils or separate modules
 
 ## General Srv config
 
-# Initialize flask constructor?
+# Initialize flask constructor 
 app = Flask(__name__)
-db_conn, sql_cursor_obj = connect_db()
-logging.basicConfig(filename='test.log', level=logging.INFO)
 
-# Check out more about decorators? 
+# Disable db connection for now
+#db_conn, sql_cursor_obj = connect_db()
+
+# Enable logging to /tmp/test.log
+#logging.basicConfig(filename='/tmp/test.log', level=logging.INFO)
+
+# Login page 
 @app.route('/')
-    
-def main():
+def login_page():
      return render_template('login_template.html')
 
-@app.route('/test')
-    
-def test_show_text():
-    return "You tried to login so hard, but in the end it doesn't matter."
-
+# Register page
 @app.route('/register')
-
 def register_user():
     return render_template('register_template.html')
-
-# Save user to DB, etc, generate user secret
-# @app.route('/reg_success')
-# def reg_redirect():
-#     return "Hello there, mr/mrs [username]! Welcome to our mega-high-tech door opener. Please don't click anything yet."
-
-# def reg_redirect():
-#     return "Hello there, mr/mrs [username]! Welcome to our mega-high-tech door opener. Please don't click anything yet."
-
     
 # Route user to authentication and show either wrong creds or home page
 @app.route('/login_attempt', methods = ['GET']) #
-    
 def login_attempt():
+
+    return "This page is temporarily disabled due to DB concerns"
+
     # usr = request.data['username']
-    usr_name = request.args['username']
-    usr_pass = request.args['pass']
-    stub = try_login_user(sql_cursor_obj,usr_name, usr_pass)
-    if (stub):
-        return render_template('home_page.html')
-    else:
-        return render_template('error_login.html')
+#    usr_name = request.args['username']
+#    usr_pass = request.args['pass']
+
+ #   stub = try_login_user(sql_cursor_obj,usr_name, usr_pass)
+ #   if (stub):
+ #       return render_template('home_page.html')
+ #   else:
+ #       return render_template('error_login.html')
     # print(type(request))
     # username = ""
     # our_utils.get_user_pass_db(sql_cursor_obj,username)
     # return "Trying to log you in but our potatoes are very hot. Just another eon please."
 
+# Route user to registration success and home page or show error message
 # @app.route('/register_attempt')
 
-# def reg_attempt():
-#     return "Some registration is going on."
-    
-# # Route for logged in users
-# @app.route('/home')
+# Route for logged in users
+@app.route('/home')
 
-# def show_personal_homepage():
-#     return "This is your happy place."
+def show_personal_homepage():
+     return "This is your happy place."
+
+
+app.run(host="0.0.0.0",port=5000)
