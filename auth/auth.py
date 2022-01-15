fc800d3d20a6c03b041372425232ab320a9a1121
Angel Yankov <angel@studyhub.bg>
2022-01-15 16:48:37 +0200

    Add mroe skeleton APIs for data signing

--- auth/auth.py
+++ auth/auth.py
@@ -3,20 +3,7 @@
 # More like a glorified API for valdiating ECDSA signed data
 from ecdsa import SigningKey
 
-
 # Mostly todos
-
-# Validate a ecdsa signed base64 string?
-def validate_string(src, verifying_key):
-    # Do ecdsa magic
-    s = 30
-    return "invalid, mid construction"
-
-# Same but for binary data?
-def validate_data(src, verifying_key):
-    s = 40
-    return 0
-
 # Generate a temporary key pair to sign some stuff for testing
 def generate_sign_key_pair_test(): 
     sk = SigningKey.generate() # Use some nist curve?
@@ -29,6 +16,33 @@ def generate_sign_key_pair_test():
 #    assert vk.verify(signature, b"test message")
 
     return sk
+
+# Verify a chunk of binary data signed wit ha key
+## Data should be encoded to UTF-8 or sth?
+def verify_signed_string_data(vk, data, signature):
+    try:
+        vk.verify(signature,data.encode())
+        print("Verification okay, calling api")
+    except:
+        print("Verification Failed")
+
+# Verify a chunk of binary data signed wit ha key
+## How do you encode binary data for ecdsa API?
+def verify_signed_binary_data(vk, data, signature):
+    try:
+        vk.verify(signature,data.encode())
+        print("Verification okay, calling api")
+    except:
+        print("Verification Failed")
+
+# Sign a string with ECC key, return signature
+def ecdsa_sign_string(sign_key, data):
+    sig = sign_key.sign(data.encode())
+    return sig
+
+def ecdsa_sign_binary_data(sign_key, data):
+    sig = sign_key.sign(data)
+    return sig
 
 def verification_demo():
     k = generate_sign_key_pair_test()
@@ -45,5 +59,3 @@ def verification_demo():
     except:
         print("Verification Failed")
 
-
-verification_demo()
